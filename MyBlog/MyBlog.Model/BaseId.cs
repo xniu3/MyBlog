1189e0f6e46a89a0186007786a33246e2ce23ed7
xinhaoniu <xniu3@ualberta.ca>
2022-09-04 10:48:32 -0600

    Added MyBlog.Model

---
+++ MyBlog/MyBlog.Model/BaseId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+using SqlSugar;
+namespace MyBlog.Model
+{
+    public class BaseId
+    {
+        [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
+        public int Id { get; set; }
+    }
+}
