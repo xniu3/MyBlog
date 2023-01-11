af9ed0b73910d9a315c2d43e64e8abccb372b074
xinhaoniu <xniu3@ualberta.ca>
2023-01-11 13:47:45 -0700

    First Skeleton

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'HomePage' });
+});
+
+router.get('/reg', function(req, res, next) {
+  res.render('reg', { title: 'Register' });
+});
+
+router.post('/reg', function (req,res,next){
+});
+
+
+router.get('/login', function(req,res,next){
+  res.render('login', {title: "登录"});
+});
+
+router.post('/login', function(req,res,next){
+});
+
+router.get('/post', function(req,res,next){
+  res.render('post', {title: "发表"})
+});
+
+router.post('/post', function(req,res,next){
+});
+
+router.get('/logout', function(req,res,next){
+});
+
+module.exports = router;
